fb67f70910d9dc83b9a313253062217e679fad47
David Brabbins <david.brabbins@outlook.com>
2017-10-12 10:09:51 -0700

    ADD conditional to call font awesome based on settings

--- bbpress-pencil-unread.php
+++ bbpress-pencil-unread.php
@@ -12,21 +12,21 @@ Domain Path: /languages
 
 class bbP_Pencil_Unread {
 	/** Version ***************************************************************/
-	
+
         /**
 	 * @public string plugin version
 	 */
 	public $version = '1.3.0';
-        
+
 	/**
 	 * @public string plugin DB version
 	 */
 	public $db_version = '108';
-	
+
 	/** Paths *****************************************************************/
-	
+
         public $file = '';
-	
+
 	/**
 	 * @public string Basename of the plugin directory
 	 */
@@ -35,29 +35,29 @@ class bbP_Pencil_Unread {
 	 * @public string Absolute path to the plugin directory
 	 */
 	public $plugin_dir = '';
-        
+
 	/**
 	 * @public name of the var used for plugin's actions
 	 */
 	public $action_varname = 'bbppu_action';
-        
+
 	/**
 	 * meta keys
 	 */
-    public $options_metaname = 'bbppu_options'; // plugin's options (stored in wp_options) 
+    public $options_metaname = 'bbppu_options'; // plugin's options (stored in wp_options)
 	public $topic_readby_metaname = 'bbppu_read_by'; // contains an array of user IDs having read that post (stored in postmeta)
 
     public $qvar = 'bbppu';
-    
+
     public $donate_link = 'http://bit.ly/gbreant';
-    
+
     public $query_time = null;
 
 	/**
 	 * @var The one true Instance
 	 */
 	private static $instance;
-        
+
 	/**
 	 * Main bbPress Pencil Unread Instance Instance
 	 *
@@ -73,75 +73,76 @@ class bbP_Pencil_Unread {
 		}
 		return self::$instance;
 	}
-        
+
 	/**
 	 * A dummy constructor to prevent from being loaded more than once.
 	 *
 	 */
 	private function __construct() { /* Do nothing here */ }
-        
+
 	function setup_globals() {
 		/** Paths *************************************************************/
 		$this->file       = __FILE__;
 		$this->basename   = plugin_basename( $this->file );
 		$this->plugin_dir = plugin_dir_path( $this->file );
 		$this->plugin_url = plugin_dir_url ( $this->file );
-        
+
         $this->options_default = array(
             'forums_marks'                          => 'on',
             'test_registration_time'                => 'on', //all items dated befoe user's first visit
             'bookmarks'                             => 'on',
+						'fontawesome'                           => 'off',
             'has_read_cache_time'                   => 5,
-            
+
         );
-        
+
         $this->options = wp_parse_args(get_option( $this->options_metaname), $this->options_default);
-        
-        
+
+
 	}
-        
+
 	function includes(){
             require( $this->plugin_dir . 'bbppu-functions.php');
             require( $this->plugin_dir . 'bbppu-settings.php');
             require( $this->plugin_dir . 'bbppu-template.php');
             //require( $this->plugin_dir . 'bbppu-ajax.php');
-        
+
             if ( $this->get_options('forums_marks') == 'on' ){
                 require( $this->plugin_dir . 'bbppu-forum-marks.php');
             }
-        
+
             if ( $this->get_options('bookmarks') == 'on' ){
                 require( $this->plugin_dir . 'bbppu-bookmarks.php');
             }
-        
+
             if (is_admin()){
             }
 	}
-	
+
 	function setup_actions(){
-            
+
             /*actions are hooked on bbp hooks so plugin will not crash if bbpress is not enabled*/
 
             add_action('bbp_init', array($this, 'load_plugin_textdomain'));     //localization - WP hook would be 'after_setup_theme'
             add_action('bbp_loaded', array($this, 'upgrade'));                  //upgrade
-            
+
             add_action('bbp_init',array(&$this,"logged_in_user_actions"));      //logged in users actions
 
             //scripts & styles
             add_action('bbp_init', array($this, 'register_scripts_styles'));
             add_action('bbp_enqueue_scripts', array($this, 'scripts_styles'));
             add_action('admin_enqueue_scripts', array($this, 'scripts_styles_admin'));
-        
+
             add_filter( 'plugin_action_links_' . $this->basename, array($this, 'plugin_bottom_links')); //bottom links
-        
+
             add_action( 'bbp_footer', array($this, 'debug_msg_total_queries_time'));
-            
+
 	}
-    
+
     function plugin_bottom_links($links){
-        
+
         $links[] = sprintf('<a target="_blank" href="%s">%s</a>',$this->donate_link,__('Donate','bbppu'));//donate
-        
+
         if (current_user_can('manage_options')) {
             $settings_page_url = add_query_arg(
                 array(
@@ -151,19 +152,19 @@ class bbP_Pencil_Unread {
             );
             $links[] = sprintf('<a href="%s">%s</a>',esc_url($settings_page_url),__('Settings'));
         }
-        
+
         return $links;
     }
-        
+
 	public function load_plugin_textdomain(){
 		load_plugin_textdomain( 'bbppu', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 	}
-        
+
 	function upgrade(){
 		global $wpdb;
- 
+
 		$version_db_key = 'bbppu-db-version';
-		
+
 		$current_version = get_option($version_db_key);
 
 		if ($current_version==$this->db_version) return false;
@@ -172,17 +173,17 @@ class bbP_Pencil_Unread {
 			//require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 			//dbDelta($sql);
 		}else{  //upgrade
-            
+
                     if ( $current_version < 105){
-                        
+
                         //remove 'bbppu_first_visit' usermetas
                         $wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->usermeta WHERE meta_key = %s",'bbppu_first_visit') );
-                        
+
                         //convert old "bbppu_marked_forum_XXX" user meta keys to a global "bbppu_marked_forums" key.
-                        
+
                         $rows = $wpdb->get_results($wpdb->prepare("SELECT user_id,meta_key,meta_value FROM $wpdb->usermeta WHERE meta_key LIKE '%s'",'bbppu_marked_forum_%'));
                         $users_marks = array();
-                        
+
                         //prepare datas
                         foreach($rows as $row){
                             $forum_id = substr($row->meta_key, strlen('bbppu_marked_forum_'));
@@ -197,42 +198,42 @@ class bbP_Pencil_Unread {
                             }
                         }
                     }
-            
+
                     if ( $current_version < 107){
 
                         $rows = $wpdb->get_results($wpdb->prepare("SELECT * FROM $wpdb->postmeta WHERE meta_key = '%s'",$this->topic_readby_metaname));
-                        
+
                         //remove old metas (unique arrays of IDs)
                         $wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->postmeta WHERE meta_key = %s",$this->topic_readby_metaname) );
-                        
+
                         //create new metas (one meta per user ID)
                         foreach((array)$rows as $row){
                             $post_id = $row->post_id;
                             $user_ids = maybe_unserialize($row->meta_value);
-                            
+
                             foreach((array)$user_ids as $user_id){
                                 add_post_meta($post_id,$this->topic_readby_metaname,$user_id);
                             }
-                        }                        
-                        
+                        }
+
                     }
-            
+
                     //remove 'bbppu_forums_visits' usermetas
                     //TO FIX could be done for v1.2.3, but wait a little that we will not revert that stuff enabling removing it.
                     //$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->usermeta WHERE meta_key = %s",'bbppu_forums_visits') );
-                    
-            
+
+
                 }
 
 		//update DB version
 		update_option($version_db_key, $this->db_version );
 	}
-        
+
 	function logged_in_user_actions(){
 
             if(!is_user_logged_in()) return false;
-        
-            
+
+
 
             //set classes for forums, topics, and subforums links.
             add_filter('bbp_get_forum_class', array(&$this,"post_status_class"),10,2);
@@ -252,9 +253,9 @@ class bbP_Pencil_Unread {
             //queries
             add_filter('query_vars', array(&$this,'register_query_vars' ));
             add_action( 'pre_get_posts', array($this, 'filter_query'));
-        
+
 	}
-    
+
     function register_query_vars($vars) {
         $vars[] = $this->qvar;
         return $vars;
@@ -262,46 +263,45 @@ class bbP_Pencil_Unread {
 
     // Filter a query for read/unread posts if the 'bbppu' var is set
     function filter_query( $query ){
-        
+
         if ( ( $query_var = $query->get( $this->qvar ) ) && ( $user_id = get_current_user_id() ) ){
-            
+
             $meta_query = $query->get('meta_query');
-            
+
             switch($query_var){
                 case 'read':
-                    
+
                     $meta_query[] = array(
                       'key' => $this->topic_readby_metaname,
                       'value' => $user_id,
                       'compare' => '=',
                     );
-                    
+
                 break;
                 case 'unread':
-                    
+
                     $meta_query[] = array(
                       'key' => $this->topic_readby_metaname,
                       'value' => $user_id,
                       'compare' => '!=',
                     );
-                    
+
                 break;
             }
-            
+
             $query->set('meta_query', $meta_query);
-            
+
         }
         return $query;
-        
+
     }
 
 	function register_scripts_styles(){
             wp_register_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',false,'4.3.0');
             wp_register_style('bbppu', $this->plugin_url . '_inc/css/bbppu.css',false,$this->version);
-            wp_register_script('bbppu', $this->plugin_url . '_inc/js/bbppu.js',array('jquery'),$this->version);
 	}
 	function scripts_styles(){
-	
+
             //SCRIPTS
 
             wp_enqueue_script('bbppu');
@@ -315,28 +315,39 @@ class bbP_Pencil_Unread {
             wp_localize_script('bbppu','bbppuL10n', $localize_vars);
 
             //STYLES
-            wp_enqueue_style('font-awesome');
-            wp_enqueue_style('bbppu');
+						if ( $this->get_options( 'fontawesome' ) == 'off' ) :
+
+							wp_enqueue_style( 'font-awesome' );
+
+						endif;
+
+						wp_enqueue_style( 'bbppu' );
+
 	}
-    
+
     //checks if the current page is the bbppu settings page
     function is_bbppu_admin(){
         $screen = get_current_screen();
         if( $screen && ($screen->id == 'settings_page_bbppu') ) return true;
     }
-    
+
 	function scripts_styles_admin(){
-        
+
             if ( !$this->is_bbppu_admin() ) return;
-	
+
+						wp_register_script( 'bbppu-admin-js', $this->plugin_url . '_inc/js/bbppu-admin.js', array( 'jquery' ) );
+
             //SCRIPTS
+						wp_enqueue_media();
+						wp_enqueue_script('bbppu-admin-js');
 
             //STYLES
             wp_enqueue_style('font-awesome');
+						wp_enqueue_style('thickbox');
 	}
 
 	function new_topic_backend($post_id){
-		
+
             // Bail if doing an autosave
             if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
                             return $post_id;
@@ -347,7 +358,7 @@ class bbP_Pencil_Unread {
             if (get_post_type( $post_id )!=bbp_get_topic_post_type())
                             return;
             // Bail if current user cannot edit this post
-            $post_obj = get_post_type_object( get_post_type( $post_id ) ); 
+            $post_obj = get_post_type_object( get_post_type( $post_id ) );
             if ( !current_user_can( $post_obj->cap->edit_post, $post_id ) )
                             return $post_id;
 
@@ -357,28 +368,28 @@ class bbP_Pencil_Unread {
 
             $this->new_topic($topic_id,$forum_id);
 	}
-    
-        
+
+
 	/**
 	 * Runs when a new topic is posted.
 	 * @param type $topic_id
 	 * @param type $forum_id
 	 * @param type $anonymous_data
-	 * @param type $topic_author 
+	 * @param type $topic_author
 	 */
-	
+
 	function new_topic($topic_id, $forum_id, $anonymous_data=false, $topic_author=false){
             $this->update_topic_read_by($topic_id,$topic_author);
 	}
-        
-        
+
+
 	/**
          * Runs when a new reply is posted.
          * @param type $reply_id
          * @param type $topic_id
          * @param type $forum_id
          * @param type $anonymous_data
-         * @param type $reply_author 
+         * @param type $reply_author
          */
 	function new_reply($reply_id, $topic_id, $forum_id, $anonymous_data=false, $reply_author=false){
             //delete metas for users who had that post read
@@ -386,81 +397,81 @@ class bbP_Pencil_Unread {
             $this->update_topic_read_by($topic_id,$reply_author);
 
 	}
-        
+
 	function new_reply_backend($post_id){
-        
+
             if ( !is_admin() ) return;
-        
+
             // Bail if doing an autosave
             if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return $post_id;
             // Bail if not a post request
             if ( 'POST' != strtoupper( $_SERVER['REQUEST_METHOD'] ) ) return $post_id;
-        
+
             // Bail if post_type do not match
             if (get_post_type( $post_id )!=bbp_get_reply_post_type()) return;
-        
+
             // Bail if current user cannot edit this post
-            $post_obj = get_post_type_object( get_post_type( $post_id ) ); 
+            $post_obj = get_post_type_object( get_post_type( $post_id ) );
             if ( !current_user_can( $post_obj->cap->edit_post, $post_id ) ) return $post_id;
-        
+
             $reply_id = bbp_get_reply_id($post_id);
             $topic_id = (isset($_POST['post_parent'])) ? bbp_get_topic_id($_POST['post_parent']) : null;
             $forum_id =  (isset($_POST['bbp_forum_id']))  ? bbp_get_forum_id($_POST['bbp_forum_id']) : null;
-        
+
             $this->new_reply($reply_id,$topic_id,$forum_id);
 	}
-	
+
 	function update_current_topic_read_by(){
             /*do not be too strict in the conditions since it makes BuddyPress group forums ignore the function eg. is_single(), etc.*/
             if (get_post_type( )!=bbp_get_topic_post_type()) return false;
             return $this->update_topic_read_by();
 	}
-    
+
     function reset_topic_read_by($topic_id=false){
             //get topic id (even if it's a reply)
             $topic_id = bbp_get_topic_id($topic_id);
             return delete_post_meta($topic_id, $this->topic_readby_metaname);
     }
-    
+
 	function update_topic_read_by($topic_id=false,$user_id=false){
-            
+
             //validate user
             if(!$user_id) $user_id = get_current_user_id();
             if (!get_userdata( $user_id )) return false;
 
             //get topic id (even if it's a reply)
             $topic_id = bbp_get_topic_id($topic_id);
-        
+
             $read_by = $this->get_topic_readers($topic_id);
-        
+
             //check topic
             if (get_post_type( $topic_id )!=bbp_get_topic_post_type()) return false;
-        
+
             if ( $read_by && in_array($user_id,$read_by) ) return true; //already set
 
             return add_post_meta( $topic_id, $this->topic_readby_metaname, $user_id );
 	}
 
 	function post_status_class($classes,$post_id){
-            
+
             //TO FIX check allowed post types
-            
+
             $post_type = get_post_type( $post_id );
             $is_read = $this->has_user_read($post_id);
-            
+
             $classes[]='bbppu-hentry';
-            
+
             if (!$is_read){
                     $classes[]='bbppu-unread';
             }else{
                     $classes[]='bbppu-read';
             }
-            
+
             return $classes;
 
 
 	}
-        
+
 
 
     /*
@@ -498,16 +509,16 @@ class bbP_Pencil_Unread {
     }
 
     function has_user_read_all_forum_topics( $forum_id,$user_id=false ){
-        
+
         //check : http://wordpress.stackexchange.com/questions/239603/how-to-speed-up-a-complex-wp-query
 
         if(!$user_id) $user_id = get_current_user_id();
         if(!$user_id) return true;
-        
+
         $time_start = microtime( true ); //for debug
         $debug_transient_message = ' (from cache)';
         $topics_total = '-';
-        
+
         //TO FIX TO CHECK use wp_cache_set instead of transients ?
         $transient_name = sprintf('bbppu_has_read_%s_user_%s',$forum_id,$user_id);
         $transient_duration = $this->get_options('has_read_cache_time');
@@ -534,7 +545,7 @@ class bbP_Pencil_Unread {
 
             if ( $skip_timestamp = $this->get_skip_timestamp($user_id,$forum_id) ){
                 $skip_time = date_i18n( 'Y-m-d H:i:s', $skip_timestamp ); //mysql format
-                //TO FIX should we query the 'post_date' field instead of this; which slows down the query ?  
+                //TO FIX should we query the 'post_date' field instead of this; which slows down the query ?
                 //We must be sure it has the same value than '_bbp_last_active_time' postmeta.
                 $topics_args['meta_query'][] = array(
                   'key' => '_bbp_last_active_time',
@@ -564,7 +575,7 @@ class bbP_Pencil_Unread {
                 $this->qvar => 'read'
             );
 
-            $has_read = true; //assuming we've read the forum 
+            $has_read = true; //assuming we've read the forum
 
 
 
@@ -587,12 +598,12 @@ class bbP_Pencil_Unread {
                 }
 
             }
-            
+
             //store for a few seconds
             set_transient( $transient_name, $has_read, $transient_duration );
-            
+
         }
-        
+
         $query_time = number_format( microtime( true ) - $time_start, 10 );
         self::debug_log(sprintf(' - user#%s has read all %s topics from forum#%s : %s - query time : %s',$user_id,$topics_total,$forum_id,$has_read,$query_time).$debug_transient_message);
 
@@ -630,7 +641,7 @@ class bbP_Pencil_Unread {
 
     }
 
-    function has_user_read($post_id,$user_id=false){ 
+    function has_user_read($post_id,$user_id=false){
 
         $has_read = false;
         $time_start = microtime( true ); //for debug
@@ -644,7 +655,7 @@ class bbP_Pencil_Unread {
         if ($forum_id === false) return;
 
         $post_type = get_post_type($post_id);
-        
+
         self::debug_log( sprintf('CHECK %s %s (has_user_read) for user#%s',get_post_type($post_id),$post_id,$user_id) );
 
         //skip timestamp
@@ -745,7 +756,7 @@ class bbP_Pencil_Unread {
 
     //TO FIX
     //filter list forums to add classes to the subforums links.
-    //this is a really nasty hack. 
+    //this is a really nasty hack.
     //Should be fixed when bbpress fix this ticket :
     //https://bbpress.trac.wordpress.org/ticket/2760#ticket
 
@@ -807,23 +818,23 @@ class bbP_Pencil_Unread {
             error_log($prefix.$message);
         }
     }
-    
+
     function debug_msg_total_queries_time(){
         self::debug_log("---");
         self::debug_log("TOTAL QUERY TIME:".$this->query_time);
         self::debug_log("---");
     }
 
-    
+
     function get_options($keys = null){
         return bbppu_get_array_value($keys,$this->options);
     }
-    
+
     public function get_default_option($keys = null){
         return bbppu_get_array_value($keys,$this->options_default);
     }
-    
-                
+
+
 }
 /**
  * The main function responsible for returning the one true Instance
